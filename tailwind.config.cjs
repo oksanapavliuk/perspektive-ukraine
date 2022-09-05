c944b5dcf3e376a2a7204a7522da72b388d39f33
IljaAvadiev <iavadiev@yahoo.com>
2022-09-05 22:49:59 +0200

    first header version

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,11 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors: {
+			yellow: '#fec608',
+			blue: '#099cc8',
+			darkblue: "#081f2d"
+		},
 		extend: {},
 	},
 	plugins: [],
