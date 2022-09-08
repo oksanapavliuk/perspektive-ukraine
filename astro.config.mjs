20e62cb69dfb552d472813c2d2543eb85b64613b
IljaAvadiev <iavadiev@yahoo.com>
2022-09-08 20:22:48 +0200

    prepare config for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://IljaAvadiev.github.io',
+  base: '/perspektive-ukraine',
   integrations: [tailwind({
     config: {applyBaseStyles: false}
   })]
