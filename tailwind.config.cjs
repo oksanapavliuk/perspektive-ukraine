006d4e87811e1b4e27dae6f2bb2c2c4c610ca71a
IljaAvadiev <iavadiev@yahoo.com>
2022-09-08 20:04:11 +0200

    add basic landing page content

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,7 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		colors: {
+			black: '#000000',
 			yellow: '#fec608',
 			blue: '#099cc8',
 			darkblue: "#081f2d"
