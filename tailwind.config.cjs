08462c116258fb82a37275425966996e8953908e
IljaAvadiev <iavadiev@yahoo.com>
2022-09-20 20:33:43 +0200

    dynamic container padding

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,6 +9,15 @@ module.exports = {
 			darkblue: "#081f2d"
 		},
 		extend: {},
+    	container: {
+    	  padding: {
+    	    DEFAULT: '1rem',
+    	    sm: '2rem',
+    	    lg: '4rem',
+    	    xl: '5rem',
+    	    '2xl': '6rem',
+    	  },
+    	},
 	},
 	plugins: [],
 }
