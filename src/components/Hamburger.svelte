cc27799eed8239b7b355ed7f1b5796d60effe636
IljaAvadiev <iavadiev@yahoo.com>
2022-09-25 17:17:40 +0200

    add static hamburger

---
+++ src/components/Hamburger.svelte
@@ -0,0 +1,12 @@
+<script>
+  let isOpen = false;
+</script>
+
+<button
+  on:click={() => (isOpen = !isOpen)}
+  class="relative block w-6 h-6 md:hidden"
+>
+  <span class="absolute top-1 left-0 w-6 h-0.5 bg-blue" />
+  <span class="absolute top-1 left-0 w-6 h-0.5 bg-blue translate-y-[7px]" />
+  <span class="absolute top-1 left-0 w-6 h-0.5 bg-blue translate-y-[14px]" />
+</button>
