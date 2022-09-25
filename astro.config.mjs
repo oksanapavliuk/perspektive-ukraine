cfd803c24c9014cdd8ae1c04e61e4186e2130954
IljaAvadiev <iavadiev@yahoo.com>
2022-09-25 15:58:48 +0200

    add svelte support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://IljaAvadiev.github.io',
   base: '/perspektive-ukraine',
   integrations: [tailwind({
-    config: {applyBaseStyles: false}
-  })]
+    config: {
+      applyBaseStyles: false
+    }
+  }), svelte()]
 });
