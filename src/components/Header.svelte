82406bc1b0eb61b1926b1a86f366a723a4d8a11e
IljaAvadiev <iavadiev@yahoo.com>
2022-10-05 15:58:13 +0200

    make functional hamburger

---
+++ src/components/Header.svelte
@@ -0,0 +1,59 @@
+<script>
+  let isOpen = false;
+</script>
+
+<header>
+  <div class="h-2.5 bg-blue" />
+  <div class="h-2.5 bg-yellow" />
+  <div class="bg-darkblue">
+    <nav class="container mx-auto px-4 py-2">
+      <div class="flex items-center justify-between">
+        <a class="shrink-0" href="/perspektive-ukraine/">
+          <img src="logo.svg" class="w-36" alt="logo" />
+        </a>
+        <ul class="hidden md:flex space-x-6 list-none">
+          <li class="text-blue hover:text-yellow">
+            <a href="/perspektive-ukraine/#veranstaltungen">Veranstaltungen</a>
+          </li>
+          <li class="text-blue hover:text-yellow">
+            <a href="/perspektive-ukraine/#projekte">Projekte</a>
+          </li>
+          <li class="text-blue hover:text-yellow">
+            <a href="/perspektive-ukraine/#spenden">Spenden</a>
+          </li>
+          <li class="text-blue hover:text-yellow">
+            <a href="/perspektive-ukraine/#mitarbeiten">Mitarbeiten</a>
+          </li>
+        </ul>
+        <button
+          on:click={() => (isOpen = !isOpen)}
+          class="relative block w-6 h-6 md:hidden"
+        >
+          <span class="absolute top-1 left-0 w-6 h-0.5 bg-blue" />
+          <span
+            class="absolute top-1 left-0 w-6 h-0.5 bg-blue translate-y-[7px]"
+          />
+          <span
+            class="absolute top-1 left-0 w-6 h-0.5 bg-blue translate-y-[14px]"
+          />
+        </button>
+      </div>
+      <div class="md:hidden" class:hidden={!isOpen}>
+        <ul class="flex flex-col items-center space-y-6 py-8 list-none">
+          <li class="text-blue hover:text-yellow">
+            <a href="/perspektive-ukraine/#veranstaltungen">Veranstaltungen</a>
+          </li>
+          <li class="text-blue hover:text-yellow">
+            <a href="/perspektive-ukraine/#projekte">Projekte</a>
+          </li>
+          <li class="text-blue hover:text-yellow">
+            <a href="/perspektive-ukraine/#spenden">Spenden</a>
+          </li>
+          <li class="text-blue hover:text-yellow">
+            <a href="/perspektive-ukraine/#mitarbeiten">Mitarbeiten</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</header>
