e4f3d984a888af8d29122e92550a6f7beeafa04a
IljaAvadiev <iavadiev@yahoo.com>
2022-10-06 18:54:36 +0200

    fix colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,13 +2,14 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		colors: {
-			black: '#000000',
-			yellow: '#fec608',
-			blue: '#099cc8',
-			darkblue: "#081f2d"
+		extend: {
+			colors: {
+				black: '#000000',
+				yellow: '#fec608',
+				blue: '#099cc8',
+				darkblue: "#081f2d"
+			},
 		},
-		extend: {},
     	container: {
     	  padding: {
     	    DEFAULT: '1rem',
