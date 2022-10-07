8f3560bb5fc0894fc90a0ab13ab0d8c56cd7c179
IljaAvadiev <iavadiev@yahoo.com>
2022-10-07 18:10:42 +0200

    move to perspektive-ukraine.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://test.perspektive-ukraine.de',
+  site: 'https://perspektive-ukraine.com',
   base: '/',
   integrations: [tailwind({
     config: {
