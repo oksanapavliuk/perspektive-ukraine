20b820bc467637d0ee5f514a999f2512b24b18b5
IljaAvadiev <iavadiev@yahoo.com>
2022-10-09 23:21:08 +0200

    optimize images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,21 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://perspektive-ukraine.com',
+  vite: {
+    assetsInclude: ['**/*.JPG']
+  },
   base: '/',
   integrations: [tailwind({
     config: {
       applyBaseStyles: false
     }
-  }), svelte()]
+  }), svelte(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  })]
 });
