44070bb7bd50a227c82cc110e6200f8daf4b0a40
IljaAvadiev <iavadiev@yahoo.com>
2022-10-14 09:17:08 +0200

    switch to de

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://perspektive-ukraine.com',
+  site: 'https://perspektive-ukraine.de',
   vite: {
     assetsInclude: ['**/*.JPG']
   },
