c8bef38d9b943f6a1077bf3563c4001a8fcd6c04
IljaAvadiev <iavadiev@yahoo.com>
2022-10-29 21:12:33 +0200

    add custom breakpoints

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,6 +10,13 @@ module.exports = {
 				darkblue: "#081f2d"
 			},
 		},
+	    screens: {
+	      'sm': '640px',
+	      'md': '768px',
+	      'lg': '1024px',
+	      'xl': '1280px',
+	      '2xl': '1536px',
+	    },
     	container: {
     	  padding: {
     	    DEFAULT: '1rem',
